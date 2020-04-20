d4b17f1b315f8bd791d0b7f43a968dc6fe54d6d8
Dave Akerman <dave@sccs.co.uk>
2020-04-20 11:11:41 +0100

    Reduce GPS screen flicker

--- gps.ino
+++ gps.ino
@@ -1,5 +1,31 @@
 void ShowGPSScreen(void)
 {
+  int Row, Height, Width, Column;
+  
+  ttgo->eTFT->setTextColor(TFT_WHITE, Background(ScreenNumber));
+
+  Row = 58;
+  Height = 93;
+  Column = 1;
+  Width = 70;
+
+  // Latitude and Longitude
+  Column = 1;
+  Width = 240 - Column - 1;
+  ttgo->eTFT->drawRoundRect(Column, Row, Width, Height, 5, TFT_WHITE);
+
+  Row += Height + 6;
+  Height = 60;
+  
+  // Satellite count
+  Column = 1;
+  Width = 65;
+  ttgo->eTFT->drawRoundRect(Column, Row, Width, Height, 5, TFT_WHITE);
+
+  // Altitude
+  Column += Width + 5;
+  Width = 240 - Column - 1;
+  ttgo->eTFT->drawRoundRect(Column, Row, Width, Height, 5, TFT_WHITE);
 }
 
 
@@ -9,6 +35,10 @@ void UpdateGPSScreen(int Always)
   
   if ((GPS.LastPositionAt > 0) && (Always || (GPS.LastPositionAt != LastPositionAt)))
   {
+    static double LastLatitude=-999;
+    static double LastLongitude=-999;
+    static long LastAltitude=-999;
+    static int LastSatellites=-1;
     char Line[32];
     int Row, Height, Width, Column;
     
@@ -28,13 +58,20 @@ void UpdateGPSScreen(int Always)
     // Latitude and Longitude
     Column = 1;
     Width = 240 - Column - 1;
-    ttgo->eTFT->drawRoundRect(Column, Row, Width, Height, 5, TFT_WHITE);
-    sprintf(Line, "%.5lf", GPS.Longitude);
-    ttgo->eTFT->setTextDatum(TC_DATUM);
-    ttgo->eTFT->drawString(Line, Column+Width/2, Row+5, 6);
-    ttgo->eTFT->setTextDatum(BC_DATUM);
-    sprintf(Line, "%.5lf", GPS.Latitude);
-    ttgo->eTFT->drawString(Line, Column+Width/2, Row+Height-1, 6);
+
+    if ((GPS.Latitude != LastLatitude) || (GPS.Longitude != LastLongitude))
+    {
+      LastLatitude = GPS.Latitude;
+      LastLongitude = GPS.Longitude;
+      
+      ttgo->eTFT->fillRect(Column+2, Row+2, Width-4, Height-4, Background(ScreenNumber));
+      sprintf(Line, "%.5lf", GPS.Latitude);
+      ttgo->eTFT->setTextDatum(TC_DATUM);
+      ttgo->eTFT->drawString(Line, Column+Width/2, Row+5, 6);
+      ttgo->eTFT->setTextDatum(BC_DATUM);
+      sprintf(Line, "%.5lf", GPS.Longitude);
+      ttgo->eTFT->drawString(Line, Column+Width/2, Row+Height-1, 6);
+    }
 
     Row += Height + 6;
     Height = 60;
@@ -42,20 +79,29 @@ void UpdateGPSScreen(int Always)
     // Satellite count
     Column = 1;
     Width = 65;
-    ttgo->eTFT->drawRoundRect(Column, Row, Width, Height, 5, TFT_WHITE);
-    ttgo->eTFT->setTextDatum(MC_DATUM);
-    ttgo->eTFT->drawNumber(GPS.Satellites, Column+Width/2, Row+Height/2-1, 6);
-    ttgo->eTFT->setTextDatum(BC_DATUM);
-    ttgo->eTFT->drawString("Sats", Column+Width/2, Row+Height-3, 1);
-
+    if (GPS.Satellites != LastSatellites)
+    {
+      LastSatellites = GPS.Satellites;
+      
+      ttgo->eTFT->fillRect(Column+2, Row+2, Width-4, Height-24, Background(ScreenNumber));
+      ttgo->eTFT->setTextDatum(MC_DATUM);
+      ttgo->eTFT->drawNumber(GPS.Satellites, Column+Width/2, Row+Height/2-1, 6);
+      ttgo->eTFT->setTextDatum(BC_DATUM);
+      ttgo->eTFT->drawString("Sats", Column+Width/2, Row+Height-3, 1);
+    }
+    
     // Altitude
     Column += Width + 5;
     Width = 240 - Column - 1;
-    ttgo->eTFT->drawRoundRect(Column, Row, Width, Height, 5, TFT_WHITE);
-    ttgo->eTFT->setTextDatum(MC_DATUM);
-    sprintf(Line, "%ld m", GPS.Altitude);
-    ttgo->eTFT->drawString(Line, Column+Width/2, Row+Height/2, 6);
-
+    if (GPS.Altitude != LastAltitude)
+    {
+      LastAltitude = GPS.Altitude;
+      ttgo->eTFT->fillRect(Column+2, Row+2, Width-4, Height-4, Background(ScreenNumber));
+      ttgo->eTFT->setTextDatum(MC_DATUM);
+      sprintf(Line, "%ld m", GPS.Altitude);
+      ttgo->eTFT->drawString(Line, Column+Width/2, Row+Height/2, 6);
+    }
+    
     LastPositionAt = GPS.LastPositionAt;
   }
 }
